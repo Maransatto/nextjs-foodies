6e19cf1f14a84426970650e8049ab3e1edac4af8
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-07 15:55:49 -0300

    feat: 455. Throwing not found errors for individual meals

---
+++ app/meals/[mealSlug]/not-found.js
@@ -0,0 +1,8 @@
+export default function MealDetailNotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal not Found</h1>
+      <p>Unfortunatelly, we could not find the requested page or meal data.</p>
+    </main>
+  );
+}
--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default async function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
 
   return (
     <>
