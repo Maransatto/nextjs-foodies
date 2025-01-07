bf972827e83479e324e7e1b5e40aabb143889255
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-07 11:43:26 -0300

    feat: 442. Using more custom components

---
+++ components/main-header/main-header.js
@@ -0,0 +1,31 @@
+import logoImg from "@/assets/logo.png";
+import Link from "next/link";
+
+import Image from "next/image";
+import MainHeaderBackground from "./main-header-background";
+import classes from "./main-header.module.css";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logoImg} alt="A Plate with food on it." priority />
+          Next Level Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
